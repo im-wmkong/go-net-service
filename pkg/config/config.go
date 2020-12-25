da6725acf1bcdfea8d50bac6ebb1048d15611d9b
WmKong <im.wmkong@gmail.com>
2020-12-25 16:41:45 +0800

    merge new config

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -4,6 +4,28 @@ import "github.com/spf13/viper"
 
 var v *viper.Viper
 
+func init() {
+	// 1. 初始化 viper 库
+	v = viper.New()
+	// 2. 设置配置文件名称
+	v.SetConfigName("config.json")
+	// 3. 配置类型，支持 "json", "toml", "yaml", "yml", "properties", "props", "prop", "env", "dotenv"
+	v.SetConfigType("json")
+	// 4. 环境变量配置文件查找的路径，相对于 main.go
+	v.AddConfigPath(".")
+
+	// 5. 开始读根目录下的配置文件，读不到会报错
+	err := v.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	// 6. 设置环境变量前缀，用以区分 Go 的系统环境变量
+	v.SetEnvPrefix("viper")
+	// 7. v.Get() 时，优先读取环境变量
+	v.AutomaticEnv()
+}
+
 // IsSet 是否设置过
 func IsSet(key string) bool {
 	return v.IsSet(key)
